98e38a4634ab66e58f31d3a613e51bc48d1218c7
Victoria Palacio Fernandez <victoriapalaciof@gmail.com>
2022-07-26 17:19:49 -0300

    realice punto 3

--- js/pto3.js
+++ js/pto3.js
@@ -17,6 +17,32 @@ for(let i = 0; i < 50; i++){
     pruebaDado1.push(dado1)
     pruebaDado2.push(dado2)
 }
-document.write("<br>suma : " + sumas);
 document.write("<br>resultado dado 1: " + pruebaDado1);
 document.write("<br>resultado dado 1: " + pruebaDado2);
+document.write("<br>suma : " + sumas);
+
+document.write(`<table class="tablaDatos">
+<thead>
+    <tr>
+        <th>Resultados</th>
+        <th>Frecuencia</th>
+    </tr>
+</thead>
+<tbody>`);
+//dibujar todas las filas que necesitamos
+for(let i = 2; i <= 12; i++){
+    document.write(`<tr>`);
+    let contador = 0;
+//necesito calcular la frecuencia de apariciones de mi variable i
+    for(let posicion = 0; posicion < sumas.length; posicion++){
+        if(i === sumas[posicion]){
+            contador++;
+        }
+    }
+        document.write(`
+            <td>${i}</td>
+            <td>${contador}</td>`);
+        document.write(`</tr>`);
+}
+document.write(`</tbody>
+</table>`);
