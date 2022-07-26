8cf66e8f9932cfcf9e993b45c3e7cfd6cbdb5114
Victoria Palacio Fernandez <victoriapalaciof@gmail.com>
2022-07-26 19:20:43 -0300

    no termine punto 2

--- js/pto2.js
+++ js/pto2.js
@@ -4,5 +4,7 @@ do{
     let city = prompt("Ingrese nombre de ciudades");
     ciudades.push(city);
 }while(confirm("Desea ingresar mas ciudades?"));
-    document.write(ciudades.length)
-    document.write(ciudades.push("Paris"));
+    document.write(ciudades)
+    ciudades.push("Paris")
+    document.write(ciudades[2]);
+    ciudades.splice(2,0,"Barcelona")
